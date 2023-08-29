364e41bb0889922f461800f274ff9854522807b3
Connor Densmore <connor@cmd-designs.com>
2023-08-29 06:32:41 -0400

    Inital
    This does not work. I don't know why.

--- cp-listen-360.php
+++ cp-listen-360.php
@@ -1,8 +1,9 @@
 <?php
 /*
-Plugin Name: Listen360 Reviews Integration
+Plugin Name: Listen360 Reviews
 Description: Custom plugin to integrate Listen360 reviews into WordPress.
-Version: 1.0
+Version: 1.11
+Author: Connor Densmore
 */
 
 // Define plugin constants
@@ -10,3 +11,93 @@ define('LISTEN360_PLUGIN_DIR', plugin_dir_path(__FILE__));
 
 // Include necessary files
 require_once LISTEN360_PLUGIN_DIR . 'includes/reviews-fetcher.php'; // Create this file for fetching reviews
+
+
+// Add admin menu item
+function listen360_add_admin_menu()
+{
+  add_menu_page(
+    'Listen360 Settings',
+    'Listen360 Settings',
+    'manage_options',
+    'listen360-settings',
+    'listen360_settings_page'
+  );
+}
+add_action('admin_menu', 'listen360_add_admin_menu');
+
+// Callback function for settings page
+function listen360_settings_page()
+{
+  ?>
+  <div class="wrap">
+    <h1>Listen360 Settings</h1>
+    <form method="post" action="options.php">
+      <?php
+      settings_fields('listen360_settings');
+      do_settings_sections('listen360-settings');
+      submit_button();
+      ?>
+    </form>
+
+    <!-- Add a manual update button -->
+    <form method="post" action="">
+      <?php wp_nonce_field('listen360_manual_update', 'listen360_manual_update_nonce'); ?>
+      <input type="hidden" name="listen360_manual_update" value="true">
+      <button type="submit" class="button">Manual Update</button>
+    </form>
+  </div>
+  <?php
+}
+
+
+// Register settings and fields
+function listen360_register_settings()
+{
+  register_setting('listen360_settings', 'listen360_api_key');
+  register_setting('listen360_settings', 'listen360_organization_reference');
+
+  add_settings_section(
+    'listen360_section',
+    'Listen360 API Settings',
+    'listen360_section_callback',
+    'listen360-settings'
+  );
+
+  add_settings_field(
+    'listen360_api_key',
+    'API Key',
+    'listen360_api_key_field',
+    'listen360-settings',
+    'listen360_section'
+  );
+
+  add_settings_field(
+    'listen360_organization_reference',
+    'Organization Reference',
+    'listen360_organization_reference_field',
+    'listen360-settings',
+    'listen360_section'
+  );
+}
+add_action('admin_init', 'listen360_register_settings');
+
+// Callback for settings section
+function listen360_section_callback()
+{
+  echo 'Enter your Listen360 API settings below:';
+}
+
+// Callback for API key field
+function listen360_api_key_field()
+{
+  $api_key = get_option('listen360_api_key');
+  echo "<input type='text' name='listen360_api_key' value='$api_key' />";
+}
+
+// Callback for Organization Reference field
+function listen360_organization_reference_field()
+{
+  $organization_reference = get_option('listen360_organization_reference');
+  echo "<input type='text' name='listen360_organization_reference' value='$organization_reference' />";
+}
--- includes/reviews-fetcher.php
+++ includes/reviews-fetcher.php
@@ -4,9 +4,59 @@
 // Function to fetch and update reviews
 function listen360_fetch_reviews()
 {
-  // Implement your logic to fetch reviews from the Listen360 API here
-  // You can use wp_remote_get() to make API requests
-  // Update or insert reviews into the custom post type
+  $api_key = get_option('listen360_api_key');
+  $org_id = "2835632886631181125";
+  $organization_reference = get_option('listen360_organization_reference');
+
+  $authorization_header = 'Basic ' . base64_encode($api_key . ':X');
+  $headers = array(
+    'Authorization' => $authorization_header,
+  );
+
+  $url = 'https://app.listen360.com/organizations/' . $org_id . '/reviews.xml?per_page=20000';
+  $response = wp_remote_get($url, array('headers' => $headers));
+
+  if (is_wp_error($response)) {
+    // Handle error
+    return;
+  }
+
+  $body = wp_remote_retrieve_body($response);
+  $xml = simplexml_load_string($body);
+
+  // Loop through XML response and filter reviews by organization-reference
+  $filtered_reviews = array();
+
+  foreach ($xml->survey as $review) {
+    $review_organization_reference = (int) $review->{'organization-reference'};
+
+    if ($review_organization_reference === (int) $organization_reference) {
+      $filtered_reviews[] = $review;
+    }
+  }
+
+  // Loop through filtered reviews and update custom post type
+  foreach ($filtered_reviews as $review) {
+    $customer_name = (string) $review->{'customer-full-name'};
+    $rating = (int) $review->{'recommendation-likelihood'};
+    $comments = (string) $review->{'public-display-comments'};
+
+    $post_data = array(
+      'post_title' => 'Review by ' . $customer_name,
+      'post_content' => $comments,
+      'post_status' => 'publish',
+      'post_type' => 'listen360_review', // Adjust based on your custom post type
+    );
+
+    $post_id = wp_insert_post($post_data);
+
+    update_post_meta($post_id, 'rating', $rating);
+    // Add more custom fields here
+
+    // Attach post to categories or taxonomies if needed
+    // wp_set_post_categories($post_id, $category_ids);
+    // wp_set_object_terms($post_id, $term_ids, 'taxonomy_name');
+  }
 }
 
 // Schedule a daily event to fetch reviews
